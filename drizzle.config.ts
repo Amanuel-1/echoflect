48df0d1c073a120f526b0a4796cf7f902066525e
Amanuel-1 <amanuelgaromsa@gmail.com>
2023-09-15 17:46:13 +0300

    added drizzle and supabase

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from "drizzle-kit";
+import * as dotenv from "dotenv";
+dotenv.config({
+  path: '.env',
+});
+ 
+export default {
+  schema: "./lib/db/schema.ts",
+  out: "./drizzle",
+  driver: 'pg',
+  dbCredentials: {
+    connectionString: process.env.DB_URL ?? "",
+  }
+} satisfies Config;
