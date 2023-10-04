246949e2b527dc05e5d5b4d5a9d59131334deabd
Amanuel-1 <amanuelgaromsa@gmail.com>
2023-10-04 23:52:41 +0300

    modified the nextconfig

--- next.config.js
+++ next.config.js
@@ -3,7 +3,9 @@ const nextConfig = {
     images: {
         domains: ['lh3.googleusercontent.com','avatars.githubusercontent.com','platform-lookaside.fbsbx.com','api.dicebear.com','images.unsplash.com','media.istockphoto.com','example.com'],
       },
-     
+      env: {
+        DATABASE_URL: process.env.DATABASE_URL,
+      }
 }
  
 module.exports = nextConfig
