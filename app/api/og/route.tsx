53de5e503626f0857d1481c14e134b8006bbf99e
Amanuel-1 <amanuelgaromsa@gmail.com>
2023-11-04 00:23:20 +0300

    change

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -15,9 +15,7 @@ export const contentType = 'image/png'
 // Image generation
 export default async function Image() {
   // Font
-  const interSemiBold = fetch(
-    new URL('./Inter-SemiBold.ttf', import.meta.url)
-  ).then((res) => res.arrayBuffer())
+
  
   return new ImageResponse(
     (
@@ -41,14 +39,7 @@ export default async function Image() {
       // For convenience, we can re-use the exported opengraph-image
       // size config to also set the ImageResponse's width and height.
       ...size,
-      fonts: [
-        {
-          name: 'Inter',
-          data: await interSemiBold,
-          style: 'normal',
-          weight: 400,
-        },
-      ],
+  
     }
   )
 }
