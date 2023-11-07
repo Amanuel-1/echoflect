f53eade3adb29ea1c4663fa95229f33e1c0cb144
Amanuel-1 <amanuelgaromsa@gmail.com>
2023-11-07 09:50:34 +0300

    og changed

--- app/(site)/posts/[article]/page.tsx
+++ app/(site)/posts/[article]/page.tsx
@@ -4,6 +4,8 @@ import styles from '@/styles/markdown.module.css'
 import Image from 'next/image'
 import { Images } from '@/public/resources';
 import postStyle from '@/styles/markdown.module.css'
+import Base64 from 'base64-js';
+
 
 import ProfileCard from '@/app/components/cards/ProfileCard';
 import Box from '@/app/components/shared/Box';
@@ -66,6 +68,13 @@ export async function generateMetadata(
 
   const currentPost  =  postList[0]
  
+  const imageData = currentPost.posts.thumbnail; // Assuming this holds the base64-encoded image data
+
+  const pngData = Base64.toByteArray(imageData); // Decode the base64 string
+
+// Convert the decoded data to a PNG buffer
+const pngBuffer = new Blob([pngData], { type: 'image/png' });
+ 
   // optionally access and extend (rather than replace) parent metadata
   const previousImages = (await parent).openGraph?.images || []
  
@@ -76,7 +85,7 @@ export async function generateMetadata(
     creator:"Amauel Garomsa",
     keywords:["blog","nature","technology","crypto","data mining","art","love","football","lifestyle"],
     openGraph:{
-      images: [`${getDomain()}/api/og?title=${currentPost.posts.title}&author=${currentPost.user.name}&image=${currentPost.posts.thumbnail}`, ...previousImages],
+      images: [`${getDomain()}/api/og?title=${currentPost.posts.title}&author=${currentPost.user.name}&image=${currentPost.posts.thumbnail.replace(/=+$/, "")}`, ...previousImages],
     },
   }
 }
