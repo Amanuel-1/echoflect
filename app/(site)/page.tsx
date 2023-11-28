f78002e3b3ce312c0a635f64cc1d4794fa882a25
Amanuel-1 <amanuelgaromsa@gmail.com>
2023-11-29 01:50:45 +0300

    lintinng

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -199,7 +199,7 @@ export default  function Home() {
             <div className="flex flex-col items-start justify-center w-full h-full pr-8 mb-10 lg:mb-0 lg:w-1/2">
                 <p className="mb-2 text-base font-medium tracking-tight text-indigo-500 uppercase">Keeping you updated to your favourite</p>
                 <h2 className="text-4xl font-extrabold leading-10 tracking-tight text-stone-200 sm:text-5xl sm:leading-none md:text-6xl lg:text-5xl xl:text-6xl">Writers</h2>
-                <p className="my-6 text-lg text-gray-200">Don't just take our word for it, read from our extensive list of case studies and customer testimonials.</p>
+                <p className="my-6 text-lg text-gray-200">Dont just take our word for it, read from our extensive list of case studies and customer testimonials.</p>
                 <a href="#_" className="flex items-center justify-center px-8 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-indigo-600 border border-transparent rounded-md shadow hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo md:py-4 md:text-lg md:px-10">Start Reading Now</a>
             </div>
             <div className="w-full lg:w-1/2 py-4">
@@ -225,7 +225,7 @@ export default  function Home() {
                             <svg className="absolute left-0 w-10 h-10 text-indigo-500 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 125">
                                 <path d="M30.7 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2C12.7 83.1 5 72.6 5 61.5c0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S30.7 31.6 30.7 42zM82.4 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2-11.8 0-19.5-10.5-19.5-21.6 0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S82.4 31.6 82.4 42z"></path>
                             </svg>
-                            <p className="mt-2 text-sm text-gray-200 sm:text-base lg:text-sm xl:text-base">I can't express enough, how amazing this service has been for my company.</p>
+                            <p className="mt-2 text-sm text-gray-200 sm:text-base lg:text-sm xl:text-base">I cant express enough, how amazing this service has been for my company.</p>
                         </div>
                         <h3 className="pl-12 mt-3 text-sm font-medium leading-5 text-gray-800 truncate sm:text-base lg:text-sm lg:text-base">
                             Amanuel Garomsa
@@ -241,7 +241,7 @@ export default  function Home() {
                             <svg className="absolute left-0 w-10 h-10 text-indigo-500 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 125">
                                 <path d="M30.7 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2C12.7 83.1 5 72.6 5 61.5c0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S30.7 31.6 30.7 42zM82.4 42c0 6.1 12.6 7 12.6 22 0 11-7.9 19.2-18.9 19.2-11.8 0-19.5-10.5-19.5-21.6 0-19.2 18-44.6 29.2-44.6 2.8 0 7.9 2 7.9 5.4S82.4 31.6 82.4 42z"></path>
                             </svg>
-                            <p className="mt-2 text-sm text-gray-200 sm:text-base lg:text-sm xl:text-base">I can't express enough, how amazing this service has been for my company.</p>
+                            <p className="mt-2 text-sm text-gray-200 sm:text-base lg:text-sm xl:text-base">I cant express enough, how amazing this service has been for my company.</p>
                         </div>
 
                         <h3 className="pl-12 mt-3 text-sm font-medium leading-5 text-gray-800 truncate sm:text-base lg:text-sm lg:text-base">
